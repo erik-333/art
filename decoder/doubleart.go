2d828cd9dff962843a591294d6ddceda4395c514
emilsundkvist <sundqvistemil04@gmail.com>
2025-02-27 14:17:26 +0200

    removed unnecessary function from doubleart.go

--- decoder/doubleart.go
+++ decoder/doubleart.go
@@ -1,19 +1,5 @@
 package main
 
-import (
-	"regexp"
-	"strconv"
-)
-
-// doubleNumber doubles values found in the input string
-func doubleNumber(input string) string {
-	re := regexp.MustCompile(`\d+`)
-	return re.ReplaceAllStringFunc(input, func(match string) string {
-		num, _ := strconv.Atoi(match)
-		return strconv.Itoa(num * 2)
-	})
-}
-
 func doubleASCIIArt(input []string) []string {
 	// First decode all lines
 	decodedLines := make([]string, 0, len(input))
